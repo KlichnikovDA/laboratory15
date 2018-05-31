fdab284d0a853d52824e494eac01334617ce6d60
KlichnikovDA <spacerus99@gmail.com>
2018-05-31 17:51:15 +0500

    Реализовал коллекции.

--- laboratory15/Program.cs
+++ laboratory15/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Librarium;
 
 namespace laboratory15
 {
@@ -10,6 +11,37 @@ namespace laboratory15
     {
         static void Main(string[] args)
         {
+            GradeBook<Exam> Book1 = new GradeBook<Exam>("Иванов Иван Иванович");
+            GradeBook<Exam> Book2 = new GradeBook<Exam>("Петров Петр Петрович");
+            GradeBook<Exam> Book3 = new GradeBook<Exam>("Семенов Семен Семенович");
+
+            #region Запросы на выборку
+            Console.WriteLine("Имена студентов заданного курса:");
+            Console.WriteLine();
+            Console.WriteLine("Имена студентов, сдавших все экзамены на \"отлично\":");
+            Console.WriteLine();
+            #endregion
+
+            #region Получение счетчика
+            Console.WriteLine("Количество студентов на заданном курсе:");
+            Console.WriteLine();
+            Console.WriteLine("Количество студентов, сдавших все экзамены на \"отлично\":");
+            Console.WriteLine();
+            Console.WriteLine("Количество студентов, не сдавших хотя бы один экзамен:");
+            Console.WriteLine();
+            #endregion
+
+            #region Аггрегирование данных
+            Console.WriteLine("Средний балл за сессию студента Иванов Иван Иванович:");
+            Console.WriteLine();
+            Console.WriteLine("Средний балл за сессию студента Петров Петр Петрович:");
+            Console.WriteLine();
+            Console.WriteLine("Средний балл за сессию студента Семенов Семен Семенович:");
+            Console.WriteLine();
+            #endregion
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
         }
     }
 }
